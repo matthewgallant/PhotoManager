2e9b3f3b22aa8ec4885d9246166bc45e553f8b5f
Matt Gallant <matt@matthewgallant.me>
2020-10-30 18:36:26 -0400

    Fixed Linux Install/Uninstall Scripts

--- Linux Installer/install.sh
+++ Linux Installer/install.sh
@@ -9,16 +9,13 @@ echo "Copying Binary"
 cp photomanager /usr/bin/
 
 echo "Copying Desktop Entry"
-cp PhotoManager.desktop /usr/share/applications/
+cp photomanager.desktop /usr/share/applications/
 
 echo "Copying Icon"
 cp photomanager.png /usr/share/pixmaps/
 
 echo "Setting Execute Permission For Binary"
 chmod +x /usr/bin/photomanager
-
-echo "Setting Execute Permission For Desktop Entry"
-chmod +x /usr/share/applications/PhotoManager.desktop
 
 echo "Install Completed"
 echo "You may need to logout and back in again for PhotoManager to appear in your apps."
--- Linux Installer/uninstall.sh
+++ Linux Installer/uninstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$EUID" -ne 0 ]
-  then echo "Please Run This As Root To Install"
+  then echo "Please Run This As Root To Uninstall"
   exit
 fi
 
@@ -9,7 +9,7 @@ echo "Removing Binary"
 rm /usr/bin/photomanager
 
 echo "Removing Desktop Entry"
-rm /usr/share/applications/PhotoManager.desktop
+rm /usr/share/applications/photomanager.desktop
 
 echo "Removing Icon"
 rm /usr/share/pixmaps/photomanager.png
